001cabeb0a119ad42a234a3e2566ad04614c58df
jon-edward <arithmatlic@gmail.com>
2021-01-07 15:54:21 -0500

    simplification of exit process

--- notifier/__init__.py
+++ notifier/__init__.py
@@ -22,19 +22,13 @@ class Notifier:
         print(send_message(escaped_message, parse_mode="MarkdownV2"))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is None:
-            time_duration = datetime.now() - self.start_time
-            message = self.succeeded_message_format.format(time_duration=time_duration,
-                                                           process=id(self),
-                                                           description=self.description)
-            escaped_message = escape_specials(message)
-            print(send_message(escaped_message, parse_mode="MarkdownV2"))
-        else:
-            formatted_tb = "\n".join(traceback.format_tb(exc_tb)).strip()
-            message = self.failed_message_format.format(exc_type=exc_type.__name__,
-                                                        exc_val=exc_val,
-                                                        exc_tb=formatted_tb,
-                                                        process=id(self),
-                                                        description=self.description)
-            escaped_message = escape_specials(message)
-            print(send_message(escaped_message, parse_mode="MarkdownV2"))
+        time_duration = datetime.now() - self.start_time
+        exit_message_format = self.succeeded_message_format if exc_type is None else self.failed_message_format
+        formatted_tb = "\n".join(traceback.format_tb(exc_tb)).strip()
+        message = exit_message_format.format(time_duration=time_duration,
+                                             exc_type=exc_type.__name__,
+                                             exc_val=exc_val,
+                                             exc_tb=formatted_tb,
+                                             description=self.description)
+        escaped_message = escape_specials(message)
+        print(send_message(escaped_message, parse_mode="MarkdownV2"))
