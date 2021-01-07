b0c625cdab0aba499bfd396d65085872329ae135
jon-edward <arithmatlic@gmail.com>
2021-01-07 16:07:52 -0500

    description update

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setuptools.setup(
     name="py-telegram-notifier",
     version="0.0.1",
     author="Jonathan Townsend",
-    description="A small example package",
+    description="Sends a message to a Telegram Bot using the Telegram bot API. This can be done by the "
+                "command line, by a send_message method call, or with a context manager.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jon-edward/py-telegram-notifier",
