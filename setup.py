221396c85dd4218de09a3f35d157f6fd692a5691
jon-edward <arithmatlic@gmail.com>
2021-03-23 11:33:27 -0400

    new build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_telegram_notifier",
-    version="0.0.2-3",
+    version="0.0.2-4",
     author="Jonathan Townsend",
     description="Simple notifier utilizing the Telegram Bot API. This can be used by the " +
                 "command line, by function call, or with a context manager.",
